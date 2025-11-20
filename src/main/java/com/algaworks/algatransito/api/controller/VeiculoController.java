98af367a4da69553e0010a8490757ca812632f5b
Artur Aires <arturaires8@gmail.com>
2025-11-20 16:55:12 -0300

    Implementando regras de negócio no cadastro de veículos

--- src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/VeiculoController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.api.controller;
 
+import com.algaworks.algatransito.domain.exception.NegocioException;
 import com.algaworks.algatransito.domain.model.Veiculo;
 import com.algaworks.algatransito.domain.repository.VeiculoRepository;
 import com.algaworks.algatransito.domain.service.RegistroVeiculoService;
@@ -35,5 +36,11 @@ public class VeiculoController {
     public Veiculo cadastrar(@RequestBody Veiculo veiculo) {
         return registroVeiculoService.cadastrar(veiculo);
     }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<String> capturar(NegocioException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
 }
 
--- src/main/java/com/algaworks/algatransito/domain/service/RegistroProprietarioService.java
+++ src/main/java/com/algaworks/algatransito/domain/service/RegistroProprietarioService.java
@@ -13,6 +13,11 @@ public class RegistroProprietarioService {
 
     private final ProprietarioRepository proprietarioRepository;
 
+    public Proprietario buscar(Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .orElseThrow(() -> new NegocioException("Proprietário não encontrado"));
+    }
+
     @Transactional
     public Proprietario salvar(Proprietario proprietario) {
         boolean emailEmUso = proprietarioRepository.findByEmail(proprietario.getEmail())
@@ -20,7 +25,7 @@ public class RegistroProprietarioService {
                 .isPresent();
 
         if (emailEmUso) {
-                throw new NegocioException("Já existe um proprietário cadastrado com este e mail");
+            throw new NegocioException("Já existe um proprietário cadastrado com este e mail");
         }
 
         return proprietarioRepository.save(proprietario);
